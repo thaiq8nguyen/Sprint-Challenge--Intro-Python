799a81912dbafb799b06151e48cfad55b67dd594
Thai Nguyen <discoverylight@yahoo.com>
2020-01-17 10:08:01 -0800

    met cityreader stretch goal

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,22 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+
+class City():
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __repr__(self):
+        return F"{self.name}, {self.lat}, {self.lon}"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -16,26 +27,37 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    line_count = 0
+    with open("src/cityreader/cities.csv") as csv_file:
+        read_csv = csv.reader(csv_file, delimiter=",")
+        for row in read_csv:
+            if line_count == 0:
+                line_count += 1
+            else:
+                city = City(row[0], float(row[3]), float(row[4]))
+                cities.append(city)
+
     return cities
+
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
-for c in cities:
-    print(c)
+# for c in cities:
+#     print(c)
 
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
-# These points form the corners of a lat/lon square. Pass these latitude and 
+# These points form the corners of a lat/lon square. Pass these latitude and
 # longitude values as parameters to the `cityreader_stretch` function, along
 # with the `cities` list that holds all the City instances from the `cityreader`
-# function. This function should output all the cities that fall within the 
+# function. This function should output all the cities that fall within the
 # coordinate square.
 #
 # Be aware that the user could specify either a lower-left/upper-right pair of
@@ -60,12 +82,37 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
+coordinate_one = input("Enter lat1, lon1: ")
+coordinate_two = input("Enter lat2, lon2: ")
+
+list_one = coordinate_one.split(",")
+list_two = coordinate_two.split(",")
+
+lat_one = float(list_one[0])
+lat_two = float(list_two[0])
+lon_one = float(list_one[1])
+lon_two = float(list_two[1])
+
+
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  # TODO Ensure that the lat and lon valuse are all floats
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    # TODO Ensure that the lat and lon valuse are all floats
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
 
-  return within
+    for city in cities:
+
+        if min(lat1, lat2) <= city.lat <= max(lat1, lat2) and\
+                min(lon1, lon2) <= city.lon <= max(lon1, lon2):
+            within.append(city)
+
+    return within
+
+
+result_cities = cityreader_stretch(lat_one, lon_one, lat_two, lon_two, cities)
+
+
+for city in result_cities:
+    print(city)
